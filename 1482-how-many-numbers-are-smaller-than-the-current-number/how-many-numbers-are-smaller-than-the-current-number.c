a16e2deb533bd46477d2aee20d8e067f76372d71
SUGAKUMAR S <ssugakumar8@gmail.com>
2026-08-16 22:49:32 +0530

    Time: 6 ms (72.65%) | Memory: 12.1 MB (52.55%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.c
@@ -0,0 +1,23 @@
+int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize)
+{
+    int *ans = (int*)malloc(numsSize * sizeof(int));
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        int count = 0;
+
+        for(int j = 0; j < numsSize; j++)
+        {
+            if(nums[j] < nums[i])
+            {
+                count++;
+            }
+        }
+
+        ans[i] = count;
+    }
+
+    *returnSize = numsSize;
+
+    return ans;
+}
